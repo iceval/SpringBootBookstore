0066ec22951bd51cea4a1d925b1c714321569afd
iceval <valera.2001@mail.ru>
2023-05-09 22:25:40 +0300

    feat: add log4j2

--- src/main/java/com/iceval/bookstore/api/controllers/BookController.java
+++ src/main/java/com/iceval/bookstore/api/controllers/BookController.java
@@ -1,5 +1,6 @@
 package com.iceval.bookstore.api.controllers;
 
+import com.iceval.bookstore.BookstoreApplication;
 import com.iceval.bookstore.api.models.BookDto;
 import com.iceval.bookstore.domain.exceptions.BookErrorResponse;
 import com.iceval.bookstore.domain.exceptions.BookNotAddedException;
@@ -8,6 +9,8 @@ import com.iceval.bookstore.bussinesslogic.services.BookService;
 import com.iceval.bookstore.dataaccess.postgresql.entities.BookEntity;
 import io.swagger.annotations.Api;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,16 +20,16 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/books")
 @Api(description = "Bookstore")
 public class BookController {
 
     private final BookService bookService;
     private final ModelMapper modelMapper;
+    private static Logger logger = LogManager.getLogger(BookController.class);
 
     @Autowired
     public BookController(BookService bookService, ModelMapper modelMapper) {
@@ -36,17 +39,20 @@ public class BookController {
 
     @GetMapping()
     public List<BookEntity> getBooks(){
+        logger.info("Get books");
         return bookService.getBooks();
     }
 
     @GetMapping("/{id}")
     public BookEntity getBookById(@PathVariable("id") int id){
+        logger.info("Get book by id={}", id);
         return bookService.getBookById(id);
     }
 
     @PostMapping()
     public ResponseEntity<HttpStatus> addBook(@RequestBody @Valid BookDto bookDto,
                                           BindingResult bindingResult){
+        logger.info("Check parameters");
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
 
@@ -60,7 +66,9 @@ public class BookController {
             throw new BookNotAddedException(errorMsg.toString());
         }
 
+        logger.info("Add book");
         bookService.addBook(convertToBookEntity(bookDto));
+        logger.info("Book added");
 
         return ResponseEntity.ok(HttpStatus.OK);
 
@@ -69,6 +77,7 @@ public class BookController {
     @PutMapping("/{id}")
     public ResponseEntity<HttpStatus> updateBook(@PathVariable("id") int id, @RequestBody @Valid BookDto bookDto,
                                                  BindingResult bindingResult) {
+        logger.info("Check parameters");
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
 
@@ -81,15 +90,18 @@ public class BookController {
 
             throw new BookNotAddedException(errorMsg.toString());
         }
-
+        logger.info("Update book");
         bookService.updateBook(id, convertToBookEntity(bookDto));
+        logger.info("Book updated");
 
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteBook(@PathVariable("id") int id) {
+        logger.info("Delete book");
         bookService.deleteBook(id);
+        logger.info("Book deleted");
 
         return ResponseEntity.ok(HttpStatus.OK);
     }
